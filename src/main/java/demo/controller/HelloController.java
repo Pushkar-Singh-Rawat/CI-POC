a1ffd81994b33b8a18152368678ca3c5e21b6004
desiCoda <j.pushkarsingh@gmail.com>
2018-04-22 15:07:07 +0530

    intended the code.

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -10,10 +10,8 @@ import demo.bean.Message;
 
 @Controller
 public class HelloController {
-
     @Autowired
     private Message message;
-
     @GetMapping(path = "/hello")
     @ResponseBody
     public String message() {
