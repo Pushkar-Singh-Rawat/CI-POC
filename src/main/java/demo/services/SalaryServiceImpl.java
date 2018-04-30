a16e9f0a075dbdb02d35281b6a9522d073f0a8f3
desicoda <j.pushkarsingh@gmail.com>
2018-04-30 18:20:06 +0530

    udpated:SalaryServiceImpl.java and SalaryServiceImplTests.java: 0 days remaining message change

--- src/main/java/demo/services/SalaryServiceImpl.java
+++ src/main/java/demo/services/SalaryServiceImpl.java
@@ -14,49 +14,52 @@ public class SalaryServiceImpl implements SalaryService {
 	String day;
 	String presentDate;
 
-	String username = ""/*System.getProperty("user.name")*/;
-	
+	String username = ""/* System.getProperty("user.name") */;
+
 	@Override
 	public String showSalaryMessage() {
 		// TODO Auto-generated method stub
-		long millis=System.currentTimeMillis();  
-		date=new Date(millis);
-		day= date.toString().split("-")[2];
-		presentDate=date.toString();
-		
-		return "Today is "+date+"." +"You have "+calculateDaysToGetSalary()+" days remaining to get salary.";
-				
+		long millis = System.currentTimeMillis();
+		date = new Date(millis);
+		day = date.toString().split("-")[2];
+		if (calculateDaysToGetSalary().equalsIgnoreCase("0")) {
+			return "Today is " + date + "." + " Happy Salary Day!";
+		} else {
+			return "Today is " + date + "." + "You have " + calculateDaysToGetSalary()
+					+ " days remaining to get salary.";
+		}
 	}
 
 	@Override
 	public String calculateDaysToGetSalary() {
-		long millis=System.currentTimeMillis();  
-		date=new Date(millis);
+		long millis = System.currentTimeMillis();
+		date = new Date(millis);
 		java.util.Date today = new java.util.Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(today);
-		day= date.toString().split("-")[2];
+		day = date.toString().split("-")[2];
 		calendar.add(Calendar.MONTH, 1);
-		calendar.set(Calendar.DAY_OF_MONTH, 1); 
-		do{
-		       calendar.add(Calendar.DATE, -1);
-		   } while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
-		           || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ); 
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		do {
+			calendar.add(Calendar.DATE, -1);
+		} while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+				|| calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY);
 		java.util.Date lastDayOfMonth = calendar.getTime();
-		    DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		    System.out.println("Today: " + sdf.format(today));
-		    System.out.println("Last Day of Month: " +sdf.format(lastDayOfMonth));
-		int presentDay=Integer.valueOf(day);
-		int lastDay=Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
-		int remainingDays=lastDay-presentDay;
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Today: " + sdf.format(today));
+		System.out.println("Last Day of Month: " + sdf.format(lastDayOfMonth));
+		int presentDay = Integer.valueOf(day);
+		int lastDay = Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
+		int remainingDays = lastDay - presentDay;
+
 		return String.valueOf(remainingDays);
 	}
 
 	@Override
 	public String displayUserName() {
 		// TODO Auto-generated method stub
-		
+
 		return null;
 	}
-	
+
 }
--- src/test/java/demo/services/SalaryServiceImplTests.java
+++ src/test/java/demo/services/SalaryServiceImplTests.java
@@ -11,70 +11,74 @@ import java.util.GregorianCalendar;
 
 import org.junit.Before;
 import org.junit.Test;
+
 public class SalaryServiceImplTests {
 
-
 	private SalaryServiceImpl salaryServiceImpl;
-    private String day;
-    private Date date;
+	private String day;
+	private Date date;
+
 	@Before
-	public void setup(){
-		salaryServiceImpl=new SalaryServiceImpl();
+	public void setup() {
+		salaryServiceImpl = new SalaryServiceImpl();
 	}
-	
+
 	@Test
-	public void testShowSalaryMessage() throws Exception{
-		String username=System.getProperty("user.name");
-		
-		long millis=System.currentTimeMillis();  
-		Date date=new Date(millis);
+	public void testShowSalaryMessage() throws Exception {
+		String username = System.getProperty("user.name");
+
+		long millis = System.currentTimeMillis();
+		Date date = new Date(millis);
 		java.util.Date today = new java.util.Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(today);
-		day= date.toString().split("-")[2];
+		day = date.toString().split("-")[2];
 		calendar.add(Calendar.MONTH, 1);
-		calendar.set(Calendar.DAY_OF_MONTH, 1); 
-		do{
-		       calendar.add(Calendar.DATE, -1);
-		   } while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
-		           || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ); 
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		do {
+			calendar.add(Calendar.DATE, -1);
+		} while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+				|| calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY);
 		java.util.Date lastDayOfMonth = calendar.getTime();
-		    DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		    System.out.println("Today: " + sdf.format(today));
-		    System.out.println("Last Day of Month: " +sdf.format(lastDayOfMonth));
-		int presentDay=Integer.valueOf(day);
-		int lastDay=Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
-		int remainingDays=lastDay-presentDay;
-		String expectedMessage="Today is "+date+"." +"You have "+remainingDays+" days remaining to get salary.";
-		assertEquals(expectedMessage, salaryServiceImpl.showSalaryMessage());
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Today: " + sdf.format(today));
+		System.out.println("Last Day of Month: " + sdf.format(lastDayOfMonth));
+		int presentDay = Integer.valueOf(day);
+		int lastDay = Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
+		int remainingDays = lastDay - presentDay;
+		String expectedMessage = "Today is " + date + "." + "You have " + remainingDays
+				+ " days remaining to get salary.";
+		if (remainingDays == 0) {
+			assertEquals("Today is " + date + "." + " Happy Salary Day!", salaryServiceImpl.showSalaryMessage());
+		} else {
+			assertEquals(expectedMessage, salaryServiceImpl.showSalaryMessage());
+		}
 	}
-	
+
 	@Test
-	public void testRemainingDaysToSalary() throws Exception{
-		long millis=System.currentTimeMillis();  
-		Date date=new Date(millis);
+	public void testRemainingDaysToSalary() throws Exception {
+		long millis = System.currentTimeMillis();
+		Date date = new Date(millis);
 		java.util.Date today = new java.util.Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(today);
-		day= date.toString().split("-")[2];
+		day = date.toString().split("-")[2];
 		calendar.add(Calendar.MONTH, 1);
-		calendar.set(Calendar.DAY_OF_MONTH, 1); 
-		do{
-		       calendar.add(Calendar.DATE, -1);
-		   } while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
-		           || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ); 
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		do {
+			calendar.add(Calendar.DATE, -1);
+		} while (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+				|| calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY);
 		java.util.Date lastDayOfMonth = calendar.getTime();
-		    DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		    System.out.println("Today: " + sdf.format(today));
-		    System.out.println("Last Day of Month: " +sdf.format(lastDayOfMonth));
-		int presentDay=Integer.valueOf(day);
-		int lastDay=Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
-		int remainingDays=lastDay-presentDay;
-		
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Today: " + sdf.format(today));
+		System.out.println("Last Day of Month: " + sdf.format(lastDayOfMonth));
+		int presentDay = Integer.valueOf(day);
+		int lastDay = Integer.valueOf(sdf.format(lastDayOfMonth).toString().split("-")[2]);
+		int remainingDays = lastDay - presentDay;
+
 		assertNotNull(salaryServiceImpl.calculateDaysToGetSalary());
 		assertEquals(String.valueOf(remainingDays), salaryServiceImpl.calculateDaysToGetSalary());
-		
-	
-		
+
 	}
 }
